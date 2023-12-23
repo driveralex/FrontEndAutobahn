4e7ec78fd17eff04587a5c2840f394bcc0a2b204
Alexandre Tisserand <116080514+Alexandre-ArmTek@users.noreply.github.com>
2023-12-23 18:04:43 +0100

    add Card component

---
+++ components/Card.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type Card from '~/composables/Card' // Import your Card class
+
+const props = defineProps<{
+  card: Card
+}>()
+</script>
+
+<template>
+  <div>
+    <p>aqui{{ props.card.debugInfo }}</p>
+  </div>
+</template>
--- components/Dev.vue
+++ components/Dev.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import Card from './Card.vue'
 import croupier from '~/machines/croupier'
+import CardClass from '~/composables/Card'
 
 const { state, send } = useMachine(croupier)
 </script>
 
 <template>
   <div>
+    <Card :card="new CardClass(1, 2)" />
     <h1>Current state: {{ state.value }}</h1>
     <h1>Current context: {{ state.context }}</h1>
     <h1>Current actions: {{ state.actions }}</h1>
